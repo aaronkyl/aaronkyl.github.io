294694949b83a9218ab8522b00e32130ac4e488b
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-05-04 17:59:25 +0000

    image rotation for edonor, tech icons shrink

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    setAfterImage();
     $('#fullstack-header').click(function() {
         $('#fullstack').slideToggle('fast')
     });
@@ -8,4 +9,12 @@ $(document).ready(function() {
     $('#misc-header').click(function() {
         $('#misc').slideToggle('fast')
     });
-})
+});
+
+function setAfterImage() {
+      $('#imageSwap').fadeIn(500).html('<img class="app-screenshot" src="img/edonor-after.png" alt="eDonor After"/>').delay(2000).fadeOut(500, function () { setBeforeImage(); });
+}
+
+function setBeforeImage() {
+      $('#imageSwap').fadeIn(500).html('<img class="app-screenshot" src="img/edonor-before.png" alt="eDonor Before"/>').delay(2000).fadeOut(500, function () { setAfterImage(); });
+}
