0d6830749515a94965b63ddc92cf8e66290ffe9d
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-05-04 07:18:43 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#fullstack-header').click(function() {
+        $('#fullstack').slideToggle('fast')
+    });
+    $('#frontend-header').click(function() {
+        $('#frontend').slideToggle('fast')
+    });
+    $('#misc-header').click(function() {
+        $('#misc').slideToggle('fast')
+    });
+})
