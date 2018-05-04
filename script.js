8db6e7dd21c271e307f121031e8473bd24bf2ee2
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-05-04 18:32:14 +0000

    down arrows on header hover

--- script.js
+++ script.js
@@ -3,36 +3,70 @@ $(document).ready(function() {
     $('#fullstack-header').click(function() {
         $('#fullstack').slideToggle('fast')
     });
+    
+    $('#fullstack-header').hover(
+        function() {
+            $('#fullstack-header .down-arrows').show()
+        }, function() {
+            $('#fullstack-header .down-arrows').hide()
+        }
+    );
+    
+    $('#frontend-header').hover(
+        function() {
+            $('#frontend-header .down-arrows').show()
+        }, function() {
+            $('#frontend-header .down-arrows').hide()
+        }
+    );
+    
+    $('#misc-header').hover(
+        function() {
+            $('#misc-header .down-arrows').show()
+        }, function() {
+            $('#misc-header .down-arrows').hide()
+        }
+    );
+    
     $('#frontend-header').click(function() {
         $('#frontend').slideToggle('fast')
     });
+    
     $('#misc-header').click(function() {
         $('#misc').slideToggle('fast')
     });
+    
     $('#cs50 img, #cs50 h4').hover(
     function() {
         $('#cs50 h4').css('color', '#FFCF0B')
     }, function() {
         $('#cs50 h4').css('color', '#EAEAEA')
     });
+    
     $('#blackjack img, #blackjack h4').hover(
     function() {
         $('#blackjack h4').css('color', '#FFCF0B')
     }, function() {
         $('#blackjack h4').css('color', '#EAEAEA')
     });
+    
     $('#weather img, #weather h4').hover(
     function() {
         $('#weather h4').css('color', '#FFCF0B')
     }, function() {
         $('#weather h4').css('color', '#EAEAEA')
     });
+    
     $('#edonor #imageSwap, #edonor h4').hover(
     function() {
         $('#edonor h4').css('color', '#FFCF0B')
     }, function() {
         $('#edonor h4').css('color', '#EAEAEA')
     });
+    
+    $('#fullstack-header .down-arrows').hide()
+    
+    
 });
 
 function setAfterImage() {
