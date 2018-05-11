07b8e9caeb1f24148992aaae03c9df7a1a4ea332
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-05-11 03:46:00 +0000

    bug fix

--- static/script/script.js
+++ static/script/script.js
@@ -106,7 +106,7 @@ $(document).ready(function() {
             $(this).toggleClass('card_face_flip_relative_toggle');
             next();
         });
-        if ($('.projects-div').css('display') == 'block') {
+        if ($('#fullstack, #frontend, #misc').css('display') == 'block') {
             $(this).css('display', 'none');
         }
     });
